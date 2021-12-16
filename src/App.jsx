f6ae030f648f479688c9f77d60f0fdc82140ee0d
Keylane <keylanessilva07@gmail.com>
2021-12-16 09:47:39 -0300

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { FiSearch } from 'react-icons/fi'
+import './App.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Buscador de Cep</h1>
+    <div className='container'>
+      <h1 className='title'>Buscador de Cep</h1>
+      
+      <div className='containerInput'>
+        <input 
+          type="text"
+          placeholder='Digite o seu cep' />
+
+          <button className='buttonSearch'>
+            <FiSearch size={25} color='#FFF'/>
+          </button>
+      </div>
+
+      <main className='main'>
+        <h2>Cep: 64003715</h2>
+
+        <span>rua</span>
+        <span>bairro</span>
+        <span>cidade</span>
+        <span>estado</span>
+
+      </main>
+
     </div>
   );
 }
